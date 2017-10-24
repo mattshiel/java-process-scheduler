3dd77d39502faee856344187a2adc40f8b00e818
Matthew Shiel <mattshiel@gmail.com>
2017-10-24 10:38:50 +0100

    Added getters and setters for processes and ArrayList to hold number of processes

--- OSScheduler/src/ie/gmit/sw/Process.java
+++ OSScheduler/src/ie/gmit/sw/Process.java
@@ -1,5 +1,30 @@
 package ie.gmit.sw;
 
 public class Process {
+	
+	private String name;
+	private int burstTime;
+
+	public Process(String name, int burstTime) {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBurstTime() {
+		return burstTime;
+	}
+
+	public void setBurstTime(int burstTime) {
+		this.burstTime = burstTime;
+	}
+	
+	
 
 }
--- OSScheduler/src/ie/gmit/sw/Runner.java
+++ OSScheduler/src/ie/gmit/sw/Runner.java
@@ -1,5 +1,31 @@
 package ie.gmit.sw;
 
-public class Runner {
+import java.util.*;
 
+public class Runner {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Please enter the amount of process to be scheduled: ");
+		int numProcesses = scanner.nextInt();
+		
+		ArrayList<Process> processes = new ArrayList<Process>(numProcesses);
+		
+		System.out.println("Enter the process time and burst time for the process");
+
+		for (int i = 0; i < numProcesses; i++) {
+			System.out.print("Enter the name for process " + (i + 1) + ": ");
+			String name = scanner.next();
+			System.out.println("Enter the burst time for process " + (i + 1) + ": ");
+			int burstTime = scanner.nextInt();
+			processes.add(new Process(name, burstTime));
+		}
+
+		System.out.println("\nChoose the Scheduling Algorithm\nPress 1 for Round Robin\nPress 2 for FCFS\nPress 3 for SJF\nPress 4 to exit");
+		System.out.print("Enter choice: ");
+		int choice = scanner.nextInt();
+
+		System.out.println();
+	}
 }
